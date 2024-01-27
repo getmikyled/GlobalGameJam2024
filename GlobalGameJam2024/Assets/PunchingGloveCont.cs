8cf5b2a8f10077508982b6fef629176ed158bae9
getmikyled <mikylemosquera@gmail.com>
2024-01-26 17:46:22 -0800

    Implement Punching Glove (Loops)

---
+++ GlobalGameJam2024/Assets/PunchingGloveCont.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GlobalGameJam2024
+{
+    public class PunchingGloveController : MonoBehaviour
+    {
+        [SerializeField] float _punchDistance = 2f;
+        public float punchDistance => _punchDistance;
+        [SerializeField] float _punchStrength = 2f;
+        public float punchStrength => _punchStrength;
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.transform.CompareTag("Player"))
+            {
+                Vector2 forceDirection = other.transform.position - transform.position;
+                forceDirection.Normalize();
+
+                other.rigidbody.AddForce(forceDirection * punchStrength, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+}
