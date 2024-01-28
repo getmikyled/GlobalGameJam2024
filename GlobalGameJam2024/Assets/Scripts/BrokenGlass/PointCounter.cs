d3fdf8919963a81124f694c79707ac6606923d4c
Meghana Indukuri <66529359+meghanai28@users.noreply.github.com>
2024-01-27 22:14:18 -0800

    minor bug fix

--- GlobalGameJam2024/Assets/Scripts/BrokenGlass/PointCounter.cs
+++ GlobalGameJam2024/Assets/Scripts/BrokenGlass/PointCounter.cs
@@ -9,18 +9,16 @@ using GlobalGameJam2024;
 public class PointCounter : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI dist;
-    [SerializeField] GameObject player;
-
-    // Start is called before the first frame update
-    void Start()
+    PlayerCharacterController player;
+    private void Awake()
     {
-        
+        player = GameObject.Find("Player").GetComponent<PlayerCharacterController>(); // intialize basically
     }
 
     // Update is called once per frame
     void Update()
     {
-        int points = player.GetComponent<PlayerCharacterController>().points; // Assuming points is a public variable in the Movement script
+        int points = player.points; // Assuming points is a public variable in the Movement script
         dist.text = points.ToString();
     }
 }
