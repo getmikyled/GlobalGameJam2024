f2d2abbdfd7436ca9f13556351309e50ec3633dc
getmikyled <mikylemosquera@gmail.com>
2024-01-27 21:58:37 -0800

    Punching Glove Jokes

---
+++ GlobalGameJam2024/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using GlobalGameJam2024;
+
+public class GameOverScreen : MonoBehaviour
+{
+    PlayerCharacterController player;
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerCharacterController>(); // intialize basically
+    }
+    public void Setup()
+    {
+
+        gameObject.SetActive(true);
+        Debug.Log("GameOver");
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
